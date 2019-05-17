fb649ef7e75b6bbe5bce8eec40dac578ea9a3387
Aamer Sohel <aamersohel7@gmail.com>
2019-05-17 17:00:21 +0530

    Initial commit

---
+++ src/main/java/io/github/aamersohel/hellogradlepublish/bin/Main.java
@@ -0,0 +1,9 @@
+package io.github.aamersohel.hellogradlepublish.bin;
+
+public class Main {
+
+    public static String getAuthor() {
+        return "Aamer Sohel <aamersohel7@gmail.com> (aamersohel.github.io)";
+    }
+
+}
